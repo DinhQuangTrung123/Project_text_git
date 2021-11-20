8aa6631d7adc14128486f534a28c8340102860e8
CodeXploreRePo <CodeXplore>
2021-11-20 23:12:52 +0700

    trung update 1

--- thinh2.py
+++ thinh2.py
@@ -8,3 +8,12 @@ def tinh1GT(n):
         return 1
     return n*tinhGT(n-1)
 
+def tinhGT(n):
+    if n <= 1:
+        return 1
+    return n*tinhGT(n-1)
+
+def tinh1GT(n):
+    if n <= 1:
+        return 1
+    return n*tinhGT(n-1)
