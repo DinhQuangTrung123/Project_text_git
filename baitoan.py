9e0cd11d3cb3699c7b2adf5814e8ec4c621c5a77
CodeXploreRePo <CodeXplore>
2021-11-20 14:54:32 +0700

    firts commit

---
+++ baitoan.py
@@ -0,0 +1,9 @@
+
+a=[5,8,4,1,2,3,9]
+for i in range(len(a)):
+    for j in range(len(a)-1):
+        if a[i] > a[j]:
+            tam = a[i]
+            a[i] = a[j]
+            a[j] = tam
+print(a)
