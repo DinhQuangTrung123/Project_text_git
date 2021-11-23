ab183db5c71e2a716f1fb52b54e80229a2135241
CodeXploreRePo <CodeXplore>
2021-11-23 17:28:47 +0700

    trung update moi nhat

--- test/test_sum.py
+++ test/test_sum.py
@@ -1,7 +1,7 @@
 import baitoan
 
 def test_calculate_something():
-    assert baitoan.calculate_something(3, 6) == 9
+    assert baitoan.calculate_something(3,6) == 9
     assert baitoan.calculate_something(3, 6) == 9
     assert baitoan.calculate_something(3, 9) == 12
     assert baitoan.calculate_something(3, 7) == 10
