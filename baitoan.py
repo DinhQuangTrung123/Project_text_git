039f7143a248a976851bf411f46323e5443ef9f8
CodeXploreRePo <CodeXplore>
2021-11-23 17:09:02 +0700

    trung update moi nhat

--- baitoan.py
+++ baitoan.py
@@ -1,12 +1,6 @@
 # ======================
 # myAnotherPythonFile.py
 # ======================
-def calculate_something(a, b, c, d, e, f, g, h):
+def calculate_something(a, b):
     i = a + b
-    j = (c + d) / 2
-    k = e * f
-    m = g - h
-    # Check if i is an odd number
-    if (i % 2) == 1:
-        i += 1
-    return i + j + k + m
+    return i 
---
+++ tests/test.py
@@ -0,0 +1,7 @@
+import baitoan
+
+def test_calculate_something():
+    assert baitoan.add(3, 6) == 9
+    assert baitoan.add(3, 6) == 12
+    assert baitoan.add(3, 9) == 9
+    assert baitoan.add(3, 7) == 10
