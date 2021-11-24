e37da61797f3379647653cd9e1702c1ea3dbfcc2
CodeXploreRePo <CodeXplore>
2021-11-24 11:06:15 +0700

    add file

--- test/test_sum.py
+++ test/test_sum.py
@@ -3,6 +3,6 @@ import baitoan
 
 def test_calculate_something():
     assert baitoan.calculate_something(3, 6) == 9
-    assert baitoan.calculate_something(3, 6) == 9
+    assert baitoan.calculate_something(3, 6) == 10
     assert baitoan.calculate_something(3, 9) == 12
     assert baitoan.calculate_something(3, 7) == 10
